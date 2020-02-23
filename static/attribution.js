ebab851ed18c1f126547178333fa58b1254464cc
Katie Broida <kbroida@gmail.com>
2020-02-23 13:53:56 -0500

    Most of the way through how-to steps

--- static/attribution.js
+++ static/attribution.js
@@ -1,5 +1,12 @@
 const IMAGES = {
-    'core-memory-close-up.jpg': {
+    'agc-memory-modules': {
+        attributionLink:
+            'https://wehackthemoon.com/tech/core-rope-memory-when-computer-science-meets-girl-power',
+        attributionText:
+            'Image courtesy: Draper',
+        location: '/static/agc-memory-modules.jpg'
+    },
+    'core-memory-close-up': {
         attributionLink:
             'http://www.ricomputermuseum.org/Home/interesting_computer_items/magnetic-core-memory',
         attributionText:
@@ -13,11 +20,46 @@ const IMAGES = {
             'Photograph © Raytheon, from the files of JackPoundstone',
         location: '/static/core-rope-memory-close-up.jpg'
     },
+    'core-rope-memory-diagram': {
+        attributionLink:
+            'https://wehackthemoon.com/tech/core-rope-memory-when-computer-science-meets-girl-power',
+        attributionText:
+            'Image courtesy: Draper',
+        location: '/static/core-rope-memory-diagram.jpg'
+    },
     'core-rope-memory-in-progress': {
         attributionLink:
             'https://wehackthemoon.com/people/dan-lickly-computer-engineering',
         attributionText: 'Image courtsey: Draper/Raytheon',
         location: '/static/core-rope-memory-in-progress.jpg'
+    },
+    'margaret-hamilton-software': {
+        attributionLink: 'https://en.wikipedia.org/wiki/Margaret_Hamilton_(software_engineer)',
+        attributionText: 'Wikipedia',
+        location: '/static/margaret-hamilton-software.jpg'
+    },
+    'rope-memory-installation': {
+        attributionLink:
+            'https://wehackthemoon.com/tech/core-rope-memory-when-computer-science-meets-girl-power',
+        attributionText:
+            'Image courtsey: Draper',
+        location: '/static/rope-memory-installation.jpg'
+    },
+    'weaving-core-rope-memory': {
+        attributionLink: 'https://wehackthemoon.com/tech/core-rope-memory-when-computer-science-meets-girl-power',
+        attributionText: 'Image courtsey: Draper',
+        location: '/static/weaving-core-rope-memory.jpg'
+    },
+    'woman-demoing-finished-rope': {
+        attributionLink: 'https://wehackthemoon.com/tech/core-rope-memory-when-computer-science-meets-girl-power',
+        attributionText: 'Image courtsey: Draper',
+        location: '/static/woman-demoing-finished-rope.jpg'
+
+    },
+    'woman-weaving-memory-1': {
+        attributionLink: 'https://wehackthemoon.com/tech/core-rope-memory-when-computer-science-meets-girl-power',
+        attributionText: 'Image courtsey: Raytheon',
+        location: '/static/woman-weaving-memory-1.jpg'
     }
 };
 
